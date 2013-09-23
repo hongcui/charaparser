f79b1aa4ec2fb974ef753c5cefd3adb113cb3419
hong1.cui@gmail.com <hong1.cui@gmail.com@ae50a849-6e19-562e-f0a0-8cc7f2e2bf57>
2013-09-23 18:11:08 +0000

    add

---
+++ CharaParser/trunk/parsing-gui/src/edu/arizona/sirls/biosemantics/parsing/DiagnosisMarkerTaxonX.java
@@ -0,0 +1,97 @@
+package edu.arizona.sirls.biosemantics.parsing;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.List;
+
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerConfigurationException;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+
+//import org.dom4j.io.OutputFormat;
+import org.jdom.Document;
+import org.jdom.Element;
+import org.jdom.JDOMException;
+import org.jdom.input.SAXBuilder;
+import org.jdom.output.Format;
+import org.jdom.output.XMLOutputter;
+import org.jdom.xpath.XPath;
+import org.w3c.dom.Node;
+
+public class DiagnosisMarkerTaxonX {
+
+	static int count=0,desc_count=0;
+
+	static boolean printStructure(Element child)
+	{
+		
+		if(child.getName().equals("name")==true)
+		{
+			return true;
+		} else if(child.getChildren().size()>0)
+		{
+			List<Element> children = child.getChildren();
+			for(Element node:children)
+			{
+				if(printStructure(node)==true)
+				{
+					return true;
+				}
+			}
+		}
+		
+		return false;
+		
+	}
+	
+	@SuppressWarnings("unchecked")
+	public static void main(String[] args) throws JDOMException, TransformerException, IOException {
+
+		
+		XPath pathDescription = XPath.newInstance(".//tax:div[@type='description']");
+		
+		File unclean = new File("C:/Users/Murali/Desktop/RA1/RA-Fall 2013/Ants/unclean/");
+		for(File input:unclean.listFiles())
+		{
+		SAXBuilder builder = new SAXBuilder();
+		Document xml;		
+		xml = builder.build(input);
+		Element root = xml.getRootElement();
+		count=0;
+		
+		List<Element> descriptions = (List<Element>)pathDescription.selectNodes(root);
+		System.out.println("Input file name "+input.getName());
+		System.out.println("Root node =="+root.getName());
+		System.out.println("Total number of descriptions"+descriptions.size());
+		
+		for(Element des:descriptions)
+		{
+			List<Element> children = des.getChildren();
+			
+			for(Element child:children)
+			{
+				if(printStructure(child)==true)
+				{
+					count++;
+					des.setAttribute("type", "description_diagnosis");
+					break;
+				}
+			}
+		}
+		
+		System.out.println("Total description node changed"+count);
+
+		
+		XMLOutputter output = new XMLOutputter();
+		output.setFormat(Format.getRawFormat());
+		output.output(xml.getDocument(), new FileWriter(new File("C:/Users/Murali/Desktop/RA1/RA-Fall 2013/Ants/cleaned/"+input.getName().replaceAll("(\\.xml)", "_")+"modified.xml")));
+		System.out.println(xml);
+	}
+	}
+
+
+}
